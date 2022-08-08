77d62a230b3a3bc37eb150b7c7bfbed213967df1
baekSh1992 <p5tential@naver.com>
2022-08-08 09:48:57 +0900

    2022-08-08
    +-*/%연산자

---
+++ 01_variable/src/edu/kh/variable/ex1/practiceNote.java
@@ -0,0 +1,11 @@
+package edu.kh.variable.ex1;
+
+public class practiceNote {
+	public static void main(String[] args) {
+		public static void main(String[] args) {
+			public static void main(String[] args) {
+				
+			}
+		}
+	}
+}

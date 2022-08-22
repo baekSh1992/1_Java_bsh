4d7ad41936a9135fa35b4a97e4fcc0e8978f7596
baekSh1992 <p5tential@naver.com>
2022-08-22 09:06:11 +0900

    2022-08-22 배열 실습문제

--- 04_array/src/edu/kh/array/practice/ArrayPractice.java
+++ 04_array/src/edu/kh/array/practice/ArrayPractice.java
@@ -164,8 +164,34 @@ public class ArrayPractice {
 		
 		System.out.println(ch + "개수 : " + count);
 		
+	}
+	
+	
+		public void practice6() {
+//		사용자가 배열의 길이를 직접 입력하여 그 값만큼 정수형 배열을 선언 및 할당하고
+//		배열의 크기만큼 사용자가 직접 값을 입력하여 각각의 인덱스에 값을 초기화 하세요.
+//		그리고 배열 전체 값을 나열하고 각 인덱스에 저장된 값들의 합을 출력하세요.
+//
+//		[실행 화면]
+//		정수 : 5
+//		배열 0번째 인덱스에 넣을 값 : 4
+//		배열 1번째 인덱스에 넣을 값 : -4
+//		배열 2번째 인덱스에 넣을 값 : 3
+//		배열 3번째 인덱스에 넣을 값 : -3
+//		배열 4번째 인덱스에 넣을 값 : 2
+//		4 -4 3 -3 2
+//		총 합 : 2
+			
+		Scanner sc = new Scanner(System.in);
 		
+		int[] arr = new int[5];
 		
+		for(int i = 0; i < arr.length; i++) {
+		 System.out.print("배열 " + i + "번째 인덱스에 넣을 값 : ");
+			arr[i] = sc.nextInt();
+			
+		}
+		}
 	}
 	
 	
@@ -188,4 +214,4 @@ public class ArrayPractice {
 	
 	
 	
-}
+
--- 04_array/src/edu/kh/array/practice/ArrayPracticeRun.java
+++ 04_array/src/edu/kh/array/practice/ArrayPracticeRun.java
@@ -10,8 +10,8 @@ public class ArrayPracticeRun {
 //		ap.practice2();
 //		ap.practice3();
 //		ap.practice4();
-		ap.practice5();
-//		ap.practice6();
+//		ap.practice5();
+		ap.practice6();
 //		ap.practice7();
 //		ap.practice8();
 //		ap.practice9();
